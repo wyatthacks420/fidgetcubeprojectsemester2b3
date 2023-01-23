f59ced9bbbe8b89b64919d53c7b896e8d5b2b156
wyatthacks420 <122548054+wyatthacks420@users.noreply.github.com>
2023-01-23 11:13:15 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,9 +9,7 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 input.onButtonPressed(Button.AB, function () {
-    for (let index = 0; index < 10; index++) {
-        music.playMelody("A B C5 A G F E D ", 1250)
-    }
+    questionMark = 1
 })
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < 4; index++) {
@@ -26,5 +24,23 @@ input.onLogoEvent(TouchButtonEvent.Touched, function () {
 	
 })
 let h_or_t_heads_or_tails = 0
+let questionMark = 0
 music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
 basic.showString("hi")
+questionMark = 0
+basic.forever(function () {
+    if (questionMark == 1) {
+        basic.showLeds(`
+        . # # # .
+        # . . . #
+        . . # # .
+        . . . . .
+        . . # . .
+        `,10)
+basic.pause(100)
+        basic.clearScreen()
+        for (let index = 0; index < 10; index++) {
+            music.playMelody("A B C5 A G F E D ", 1250)
+        }
+    }
+})
